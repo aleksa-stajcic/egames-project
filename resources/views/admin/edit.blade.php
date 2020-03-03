3822c92e95deca2c138bfd509a9f91e338a61fe5
aleksa-stajcic <ethsiberiandex@hotmail.com>
2020-03-03 14:27:42 +0100

    user.edit form

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -1,28 +1,35 @@
 @include('shared.head')
 
-<table class="table table-striped table-dark">
-    <thead>
-        <tr>
-            <th scope="col">Id</th>
-            <th scope="col">Picture</th>
-            <th scope="col">Username</th>
-            <th scope="col">Email</th>
-            <th scope="col">Active</th>
-            <th scope="col">Role</th>
-            <th scope="col">Date Added</th>
-            <th scope="col">Date Modified</th>
-        </tr>
-    </thead>
-    <tbody>
-            <tr>
-                <td>{{$user->Id}}</td>
-                <td><img src="{{ asset($user->ProfileImage) }}" alt="" srcset="" width="100px" height="100px"></td>
-                <td>{{$user->Username}}</td>
-                <td>{{$user->Email}}</td>
-                <td>{{$user->IsActive}}</td>
-                <td>{{$user->RoleId}}</td>
-                <td>{{$user->DateAdded}}</td>
-                <td>{{$user->DateModified}}</td>
-            </tr>
-    </tbody>
-</table>
+<form action="" method="post" enctype="multipart/form-data">
+    @csrf
+    <div class="row">
+        <div class="col-10">
+            <strong>Username</strong>
+            <input type="text" class="form-control" id="username" name="username" placeholder="Username" value="{{$user->Username}}">
+        </div>
+        <div class="col-10">
+            <strong>Email</strong>
+            <input type="email" class="form-control" id="email" name="email" placeholder="Email" value="{{$user->Email}}">
+        </div>
+        
+        <div class="col-10">
+            <strong>Role</strong>
+            <input type="text" class="form-control" id="RoleId" name="RoleId" placeholder="RoleId" value="{{$user->RoleId}}">
+        </div>
+        <div class="col-10">
+            <strong>Active</strong>
+            <input type="text" class="form-control" id="isActive" name="isActive" placeholder="Active" value="{{$user->IsActive}}">
+        </div>
+        {{-- OPTION TO ADD AVATAR AFTER REGISTRATION --}}
+        {{-- <div class="col-10">
+            <strong>Profile image</strong>
+            <input type="file" class="form-control" id="image" name="image" placeholder="Profile picture"> 
+        </div> --}}
+        <div class="col-10 col-md-5">
+            <button class="btn egames-btn w-100" id="btnEdit" name="btnEdit" type="submit" disabled>Edit</button>
+        </div>
+        {{-- <div class="col-10 col-md-5">
+            <button class="btn egames-btn w-100" type="submit">Submit Comment</button>
+        </div> --}}
+    </div>
+</form>
