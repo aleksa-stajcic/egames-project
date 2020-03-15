16209912a9792cb6fb5680ffa3ca042a4930db35
aleksa-stajcic <ethsiberiandex@hotmail.com>
2020-03-15 14:53:27 +0100

    posts create from

---
+++ resources/views/post-create.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.front')
+@section('title')
+    Create a post
+@endsection
+@section('content')
+
+<div class="row justify-content-center">
+    <div class="post-a-comment-area mb-30 clearfix" id="reply">
+        <h4 class="mb-50">New post</h4>
+
+        <div class="contact-form-area">
+            <form action="{{ route('posts.store') }}" method="post">
+                @csrf
+                <div class="row">
+                    <div class="col-10">
+                        <input type="text" class="form-control" id="title" name="title" placeholder="Title" required>
+                    </div>
+                    <div class="col-10">
+                        <textarea type="password" class="form-control" id="post-text" name="post-text" placeholder="What is your post about?" required>
+
+                        </textarea>
+                    </div>
+                    <div class="col-10 col-md-5">
+                        <button class="btn egames-btn w-100" id="" name="" type="submit">Submit post</button>
+                    </div>
+                </div>
+            </form>
+            @if(session('msg'))
+                <div class="alert alert-warning">
+                    {{ session('msg') }}
+                </div>
+            @endif
+        </div>
+    </div>
+</div>
+
+@endsection
